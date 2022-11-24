d5b3b561a33fa2cb05b5b9c6b373403a6eed97f2
Loris Rota <lorisrotascienze234@gmail.com>
2022-11-24 22:36:20 +0100

    Proseguimento server

---
+++ Bacheca_Server/ClassiServer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bacheca_Server
+{
+    internal class ClassiServer
+    {
+    }
+
+    public class Server_Bacheca
+    {
+        private Socket socket;
+
+        public Server_Bacheca()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Start(IPAddress IP,int port)
+        {
+            IPEndPoint EndPoint = new IPEndPoint(IP, port);
+            socket.Bind(EndPoint);
+            socket.Listen(20);
+        }
+
+    }
+    public class Item
+    {
+        private string username;
+        private string board_name;
+        private bool visible;
+        private int length;
+        private string text;
+        private DateTime creation_time;
+        private DateTime fixed_date;
+
+        public Item()
+        {
+            text = "";
+            creation_time = DateTime.Now;
+        }
+        public string Username { get { return username; } set { username = value; } }
+        public string Text { get { return text; } set { text = value; } }
+        public bool Visibility { get { return visible; } set { visible = value; } }
+        public string Board { get { return board_name; } }
+        public DateTime Date { get { return fixed_date; } set { fixed_date = value; } }
+
+        public string Pack()
+        {
+            string msg = "";
+            msg += Username + "|" + Board + "|" + Visibility.ToString() + "|" + /* Lunghezza + Tipo +*/ Date.ToString() + "|" + Text;
+
+            return msg;
+        }
+        public string Unpack(string msg)
+        {
+
+            return "";
+        }
+    }
+
+    public class Board
+    {
+        string filepath;
+        string name;
+        bool prvt;
+
+        public Board()
+        {
+            filepath = "";
+            name = "MyBoard";
+        }
+        public void Create(string name, string filepath)
+        {
+            this.name = name;
+            this.filepath = filepath;
+        }
+        public void AddMemo(Item memo)
+        {
+            // Scrive il memo nel file
+        }
+    }
+}
--- Bacheca_Server/Form1.cs
+++ Bacheca_Server/Form1.cs
@@ -7,15 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Bacheca_Server
 {
     public partial class Form1 : Form
     {
+        public Server_Bacheca Server;
+        public IPAddress IP = IPAddress.Parse("127.0.0.1");
         public Form1()
         {
             InitializeComponent();
+            Server = new Server_Bacheca();
+            Server.Start(IP, 50000);
         }
+
 
     }
 }
